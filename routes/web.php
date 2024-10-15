d9eb248d269d4b9965f300c99735d8b159afb0db
Dang Thanh Trung <126983677+MonteryTrunn@users.noreply.github.com>
2024-10-15 17:09:15 +0700

    v2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('profile/{username}',[ProfileController::class,'index'])
+    ->middleware('auth','verified')->name('profile');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
